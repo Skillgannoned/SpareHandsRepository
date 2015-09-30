114057cf993e0aa3bd121658189f33ee54291411
AnotherSamPower <sampower12358@gmail.com>
2015-09-30 23:16:16 +0100

    Fixed Adverts Nav Bug
    Refactored ID’s of all navabar buttons to have nav at the end.

--- SpareHands/WebContent/resources/scripts/main.js
+++ SpareHands/WebContent/resources/scripts/main.js
@@ -2,45 +2,46 @@ var loggedIn = false;
 
 $(document).ready(function() {
 	$('#logout').hide();
-	$('#applications').hide();
-	$('#profile').hide();
-	$("#adverts").click(function(event) {
+	$('#applicationsNav').hide();
+	$('#profileNav').hide();
+	
+	$("#advertsNav").click(function(event) {
 		advertsSelected();
 	});
-	$("#applications").click(function(event) {
-		$('#adverts').addClass('inactive').removeClass('active');
-		$('#applications').addClass('active').removeClass('inactive');
-		$('#profile').addClass('inactive').removeClass('active');
+	$("#applicationsNav").click(function(event) {
+		$('#advertsNav').addClass('inactive').removeClass('active');
+		$('#applicationsNav').addClass('active').removeClass('inactive');
+		$('#profileNav').addClass('inactive').removeClass('active');
 		$('#adverts-section').hide();
 	});
-	$("#profile").click(function(event) {
-		$('#adverts').addClass('inactive').removeClass('active');
-		$('#applications').addClass('inactive').removeClass('active');
-		$('#profile').addClass('active').removeClass('inactive');
+	$("#profileNav").click(function(event) {
+		$('#advertsNav').addClass('inactive').removeClass('active');
+		$('#applicationsNav').addClass('inactive').removeClass('active');
+		$('#profileNav').addClass('active').removeClass('inactive');
 		$('#adverts-section').hide();
 	});
 
 	$("#register-login").click(function(event) {
 		$('#register-login').hide();
 		$('#logout').show();
-		$('#applications').show();
-		$('#profile').show();
+		$('#applicationsNav').show();
+		$('#profileNav').show();
 		loggedIn = true;
 	});
 
 	$("#logout").click(function(event) {
 		$('#register-login').show();
 		$('#logout').hide();
-		$('#applications').hide();
-		$('#profile').hide();
+		$('#applicationsNav').hide();
+		$('#profileNav').hide();
 		loggedIn = false;
 		advertsSelected();
 	});
 });
 
 function advertsSelected() {
-	$('#adverts').addClass('active').removeClass('inactive');
-	$('#applications').addClass('inactive').removeClass('active');
-	$('#profile').addClass('inactive').removeClass('active');
+	$('#advertsNav').addClass('active').removeClass('inactive');
+	$('#applicationsNav').addClass('inactive').removeClass('active');
+	$('#profileNav').addClass('inactive').removeClass('active');
 	$('#adverts-section').show();
 }
